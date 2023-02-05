fd3b366ef3035e56a2a6727241633e44f2a86a0b
Greg Richardson <jgrichardson67@yahoo.com>
2023-02-05 14:32:13 -0500

    Add devise and Post functionality with turb_frame

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  post 'posts/create', as: :posts
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'site#index'
 end
